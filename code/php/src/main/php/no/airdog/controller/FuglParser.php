f7a3298af3b033d473ee00b8ad05289d61521a84
hallatore <hallatore@16cdc39a-dd10-11dd-bce9-bf72c0239d3a>
2009-02-03 08:46:41 +0000

    Fuglparser og Fuglparsertest

---
+++ code/php/src/main/php/no/airdog/controller/FuglParser.php
@@ -0,0 +1,71 @@
+<?php
+
+class FuglParser
+{
+	public function FuglParser()
+	{
+	}
+	
+	public function getFuglArray($enFugl)
+	{
+		$fuglArray = split('[|]', trim($enFugl));
+		
+		if (sizeof($fuglArray) == 38)
+		{
+			return array (
+			"proveNr" => $fuglArray[0],
+			"proveDato" => $fuglArray[1],
+			"partiNr" => $fuglArray[2],
+			"klasse" => $fuglArray[3],
+			"dommerId1" => $fuglArray[4],
+			"dommerId2" => $fuglArray[5],
+			"hundId" => $fuglArray[6],
+			"slippTid" => $fuglArray[7],
+			"egneStand" => $fuglArray[8],
+			"egneStokk" => $fuglArray[9],
+			"tomStand" => $fuglArray[10],
+			"makkerStand" => $fuglArray[11],
+			"makkerStokk" => $fuglArray[12],
+			"jaktlyst" => $fuglArray[13],
+			"fart" => $fuglArray[14],
+			"stil" => $fuglArray[15],
+			"selvstendighet" => $fuglArray[16],
+			"bredde" => $fuglArray[17],
+			"reviering" => $fuglArray[18],
+			"samarbeid" => $fuglArray[19],
+			"presUpresis" => $fuglArray[20],
+			"presNoeUpresis" => $fuglArray[21],
+			"presPresis" => $fuglArray[22],
+			"reisNekter" => $fuglArray[23],
+			"reisNoelende" => $fuglArray[24],
+			"reisVillig" => $fuglArray[25],
+			"reidDjerv" => $fuglArray[26],
+			"sokStjeler" => $fuglArray[27],
+			"sokSpontant" => $fuglArray[28],
+			"appIkkeGodkjent" => $fuglArray[29],
+			"appGodkjent" => $fuglArray[30],
+			"rappInnkalt" => $fuglArray[31],
+			"rappSpont" => $fuglArray[32],
+			"premiegrad" => $fuglArray[33],
+			"certifikat" => $fuglArray[34],
+			"regAv" => $fuglArray[35],
+			"regDato" => $fuglArray[36],
+			"raseId" => $fuglArray[37]
+			);
+		}
+	}
+	
+	public function getFugllisteArray($fuglliste)
+	{
+		$fugllisteArray = split("\r\n", $fuglliste);
+		$ret = array();
+		
+		for ($i = 0; $i < sizeof($fugllisteArray); $i++)
+    	{
+    		$ret[] = $this->getFuglArray($fugllisteArray[$i]);
+    	}
+    	
+    	return $ret;
+	}
+}
+?>
---
+++ code/php/src/test/php/no/airdog/controller/FuglParserTest.php
@@ -0,0 +1,72 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+require_once str_replace('.','/','no.airdog.controller').'/FuglParser.php';
+
+class FuglParserTest extends PHPUnit_Framework_TestCase 
+{
+    function testGetFuglArray() 
+    {
+    	$hp = new FuglParser();
+    	$parseString = "50-95042|01.04.1995|L1|4|1355668|2425066|20466/90|8|1|0|1|2|3|5|4|3|2|1|2|3|4|5|6|5|4|3|2|1|2|3|4|5|6|5|4|TA|13.05.1995|348";
+        $pa = $hp->getFuglArray($parseString);
+        
+    	$this->assertEquals("50-95042", $pa["proveNr"]);
+    	$this->assertEquals("01.04.1995", $pa["proveDato"]);
+    	$this->assertEquals("L1", $pa["partiNr"]);
+    	$this->assertEquals("4", $pa["klasse"]);
+    	$this->assertEquals("1355668", $pa["dommerId1"]);
+    	$this->assertEquals("2425066", $pa["dommerId2"]);
+    	$this->assertEquals("20466/90", $pa["hundId"]);
+    	$this->assertEquals("8", $pa["slippTid"]);
+    	$this->assertEquals("1", $pa["egneStand"]);
+    	$this->assertEquals("0", $pa["egneStokk"]);
+    	$this->assertEquals("1", $pa["tomStand"]);
+    	$this->assertEquals("2", $pa["makkerStand"]);
+    	$this->assertEquals("3", $pa["makkerStokk"]);
+    	$this->assertEquals("5", $pa["jaktlyst"]);
+    	$this->assertEquals("4", $pa["fart"]);
+    	$this->assertEquals("3", $pa["stil"]);
+    	$this->assertEquals("2", $pa["selvstendighet"]);
+    	$this->assertEquals("1", $pa["bredde"]);
+    	$this->assertEquals("2", $pa["reviering"]);
+    	$this->assertEquals("3", $pa["samarbeid"]);
+    	$this->assertEquals("4", $pa["presUpresis"]);
+    	$this->assertEquals("5", $pa["presNoeUpresis"]);
+    	$this->assertEquals("6", $pa["presPresis"]);
+    	$this->assertEquals("5", $pa["reisNekter"]);
+    	$this->assertEquals("4", $pa["reisNoelende"]);
+    	$this->assertEquals("3", $pa["reisVillig"]);
+    	$this->assertEquals("2", $pa["reidDjerv"]);
+    	$this->assertEquals("1", $pa["sokStjeler"]);
+    	$this->assertEquals("2", $pa["sokSpontant"]);
+    	$this->assertEquals("3", $pa["appIkkeGodkjent"]);
+    	$this->assertEquals("4", $pa["appGodkjent"]);
+    	$this->assertEquals("5", $pa["rappInnkalt"]);
+    	$this->assertEquals("6", $pa["rappSpont"]);
+    	$this->assertEquals("5", $pa["premiegrad"]);
+    	$this->assertEquals("4", $pa["certifikat"]);
+    	$this->assertEquals("TA", $pa["regAv"]);
+    	$this->assertEquals("13.05.1995", $pa["regDato"]);
+    	$this->assertEquals("348", $pa["raseId"]);
+    }
+    
+    function testGetFugllisteArray()
+    {
+    	$parseString = '50-95042|01.04.1995|L1|4|1355668|2425066|20466/90|8|1|0|1|2|3|5|4|3|2|1|2|3|4|5|6|5|4|3|2|1|2|3|4|5|6|5|4|TA|13.05.1995|348
+    					50-95044|01.04.1995|L1|4|1355668|2425066|20466/90|8|1|0|1|2|3|5|4|3|2|1|2|3|4|9|6|5|4|3|2|1|2|3|4|5|6|5|4|TA|13.05.1995|349';
+    	
+        $hp = new FuglParser();
+        $pa = $hp->getFugllisteArray($parseString);
+        
+        $this->assertEquals("2", sizeof($pa));
+    	
+    	$this->assertEquals("50-95042", $pa[0]["proveNr"]);				// Toppen i arrayet
+    	$this->assertEquals("5", $pa[0]["presNoeUpresis"]);				// Midten
+    	$this->assertEquals("348", $pa[0]["raseId"]);					// Bunnen i arrayet
+    	
+    	$this->assertEquals("50-95044", $pa[1]["proveNr"]);				// Toppen i arrayet
+    	$this->assertEquals("9", $pa[1]["presNoeUpresis"]);				// Midten
+    	$this->assertEquals("349", $pa[1]["raseId"]);					// Bunnen i arrayet
+    }    
+}
+?>
