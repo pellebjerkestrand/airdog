02336f10746ab2336c5797da93590cc11aac8487
hinderberg <hinderberg@16cdc39a-dd10-11dd-bce9-bf72c0239d3a>
2009-02-12 12:14:29 +0000

    Brukere :)

--- trunk/code/php/src/main/php/no/airdog/index.php
+++ trunk/code/php/src/main/php/no/airdog/index.php
@@ -1,6 +1,7 @@
 <?php
-
+//Error rapportering
 error_reporting(E_ALL|E_STRICT);
+//Skru av f¿r produktet skal lanseres
 ini_set("display_errors", "on");
 
 if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
@@ -9,18 +10,28 @@ if (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
 	ini_set("include_path", ini_get("include_path") . ":../../com/:../../no/"); 
 }
 
+ini_set("include_path", ini_get("include_path").PATH_SEPARATOR."../../com/:../../no/");
+
+
+require_once '../../com/Zend/Loader.php';
+Zend_Loader::registerAutoload();
 
 require_once '../../com/Zend/Amf/Server.php';
+
 require_once 'controller/HundController.php';
-require_once 'controller/BrukerController.php';
+require_once 'controller/LoggInnController.php';
+
 
 $server = new Zend_Amf_Server();
 $server->setClass("HundController");
-$server->setClass("BrukerController");
+$server->setClass("LoggInnController");
+
 $server->setClassMap("AmfHund", "AmfHund");
 $server->setClassMap("AmfAvkom", "AmfAvkom");
-$server->setClassMap("AmfBruker", "AmfBruker");
+$server->setClassMap("AmfBruker","AmfBruker");
 
+//Bytt til true nŒr det skal lanseres
+$server->setProduction(false);
 echo($server->handle());
 
 ?>
---
+++ trunk/code/php/src/main/php/no/airdog/model/AmfBrukerRettigheter.php
@@ -0,0 +1,7 @@
+<?php
+class AmfBrukerRettigheter
+{
+public $brukerRolle="";
+public $seUI="";
+public $seLogg = "";
+}
