d9b533dbcd3ed3ef0a45303840615d7effcc9ea5
hinderberg <hinderberg@16cdc39a-dd10-11dd-bce9-bf72c0239d3a>
2009-02-17 14:55:29 +0000

    fiks path

--- trunk/code/php/src/main/php/no/airdog/index.php
+++ trunk/code/php/src/main/php/no/airdog/index.php
@@ -5,9 +5,9 @@ error_reporting(E_ALL|E_STRICT);
 ini_set("display_errors", "on");
 
 ini_set("include_path", ini_get("include_path") .
-	PATH_SEPARATOR . '../../../com/' .
-	PATH_SEPARATOR . '../../../no/' .
-	PATH_SEPARATOR . '../../../'); 
+	PATH_SEPARATOR . '../../com/' .
+	PATH_SEPARATOR . '../../no/' .
+	PATH_SEPARATOR . '../../'); 
 
 //AUTOLOADER - Setter opp automatisk loading.
 //Gj¿r at ZF sine klasser ikke trenger Œ inkluderes eller kreves.
