c9307bcf6ac701ef4f402793f7c52e94d6286dbd
hinderberg <hinderberg@16cdc39a-dd10-11dd-bce9-bf72c0239d3a>
2009-02-25 09:04:23 +0000

    Rettighetsvalidering

--- trunk/code/php/src/main/php/no/airdog/controller/database/ValiderBruker.php
+++ trunk/code/php/src/main/php/no/airdog/controller/database/ValiderBruker.php
@@ -2,16 +2,13 @@
 require_once 'Tilkobling.php';
 class ValiderBruker
 {	
-	public static function sjekkBruker($brukerEpost, $passord)
+	public static function validerBrukeren($database, $brukerEpost, $brukerPassord)
 	{
-		$tilkobling = new Tilkobling();
-		$database = $tilkobling->getTilkobling();
-		
 		$brk = $database->quoteInto('epost=?', $brukerEpost);
-		$pass = $database->quoteInto('passord=?', $passord);
+		$pass = $database->quoteInto('passord=?', $brukerPassord);
 		
 		$hent = $database->select()
-		->from('AD_bruker', array('AD_bruker.*'))
+		->from('ad_bruker', array('ad_bruker.*'))
 		->where($brk)
 		->where($pass);
 		
@@ -21,5 +18,31 @@ class ValiderBruker
 		}
 		
 		return false;
+	}	
+	
+	public static function validerBrukerRettighet($database, $brukerEpost, $brukerPassord, $klubbId, $rettighet)
+	{
+	
+		if($this->validerBruker($database, $brukerEpost, $brukerPassord))
+		{
+			$brk = $database->quoteInto('a.ad_bruker_epost=?', $brukerEpost);
+			$klb = $database->quoteInto('a.ad_klubb_id=?', $klubbId);
+			
+			$hent = $database->select()
+			->from(array('a' => 'ad_bruker_klubb_rolle_link'), array('rr.ad_rettighet_navn'))
+			->join(array('rr' => 'ad_rolle_rettighet_link'), 'rr.ad_rolle_navn = a.ad_rolle_navn', array())
+			->where($brk)
+			->where($klb);
+			
+			if($database->fetchRow($hent) != null)
+			{
+				return true;
+			}
+			
+			return false;
+		}
+		
+		return false;
+		
 	}	
 }
